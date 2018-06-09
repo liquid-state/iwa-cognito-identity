322a6f2c5bf2ee0c66e02dd71ee4f15e8bcbf2b7
Chris Hopkins <chris@liquid-state.com>
2018-06-09 16:39:17 +1000

    Add cognito configuration helpers to this package, ported from login blueprint

---
+++ src/configuration.ts
@@ -0,0 +1,45 @@
+import * as AWS from 'aws-sdk/global';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import KeyValuePlugin, { Key } from '@liquid-state/iwa-keyvalue';
+import IdentityPlugin, { IdentityStore } from '@liquid-state/iwa-identity';
+import CognitoIdentity from './identity';
+import CognitoAuthenticator from './authentication';
+import { IApp } from '@liquid-state/iwa-core/dist/app/app';
+
+const COGNITO_SETTINGS = [
+  'AWS_USER_POOL_ID',
+  'AWS_IDENTITY_POOL_ID',
+  'AWS_USER_POOL_CLIENT_ID',
+  'AWS_REGION',
+];
+
+export type setPermissionsForKeyT = (key: Key) => Key;
+
+export const configureCognito = async (app: IApp, setPermissionsForKey: setPermissionsForKeyT) => {
+  const settings = await app.configuration(...COGNITO_SETTINGS);
+
+  const { AWS_USER_POOL_ID, AWS_USER_POOL_CLIENT_ID, AWS_IDENTITY_POOL_ID, AWS_REGION } = settings;
+
+  AWS.config.update({ region: AWS_REGION });
+
+  const userPool = new CognitoUserPool({
+    UserPoolId: AWS_USER_POOL_ID,
+    ClientId: AWS_USER_POOL_CLIENT_ID,
+  });
+
+  const kv = app.use(KeyValuePlugin);
+  const idStore = new IdentityStore(kv, { setPermissionsForKey });
+
+  app
+    .use(IdentityPlugin)
+    .addProvider('cognito', new CognitoIdentity(userPool, AWS_IDENTITY_POOL_ID, idStore));
+};
+
+export const getAuthenticator = async (app: IApp) => {
+  const settings = await app.configuration(...COGNITO_SETTINGS);
+  const userPool = new CognitoUserPool({
+    UserPoolId: settings.AWS_USER_POOL_ID,
+    ClientId: settings.AWS_USER_POOL_CLIENT_ID,
+  });
+  return new CognitoAuthenticator(userPool);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as CognitoAuthenticator } from './authentication';
+export { configureCognito, getAuthenticator } from './configuration';
 
 import CognitoIdentity from './identity';
 export default CognitoIdentity;
