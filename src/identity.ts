9c5c5d90262ee1be0e60a7508540d432b98640d3
Chris Hopkins <chris@liquid-state.com>
2019-12-10 16:11:22 +1000

    Cognito user pool authentication only (#13)

--- src/identity.ts
+++ src/identity.ts
@@ -1,19 +1,15 @@
-import * as AWS from 'aws-sdk/global';
-import { CognitoIdentityCredentials } from 'aws-sdk';
-
 import { CognitoUserSession, CognitoUserPool, CognitoUser } from 'amazon-cognito-identity-js';
 
 import { IIdentity } from '@liquid-state/iwa-identity/dist/identity';
 import { IIdentityProvider } from '@liquid-state/iwa-identity/dist/manager';
-import { IIdentityStore } from '@liquid-state/iwa-identity/dist/store';
+// import { IIdentityStore } from '@liquid-state/iwa-identity/dist/store';
 
 import { Identity } from '@liquid-state/iwa-identity';
-import KVStorage, { OpaqueObject } from './storage';
+// import KVStorage, { OpaqueObject } from './storage';
 
 export type AWSServiceCredentials = {
-  accessKeyId: string;
-  secretAccessKey: string;
-  sessionToken: string;
+  user: CognitoUser;
+  session: CognitoUserSession;
 };
 
 /**
@@ -23,32 +19,24 @@ export class AWSIdentity implements IIdentity<AWSServiceCredentials> {
   public isAuthenticated: boolean;
   private identityMap = new Map<string, string>();
 
-  constructor(public name: string, public credentials: CognitoIdentityCredentials) {
+  constructor(public name: string, public credentials: AWSServiceCredentials) {
     this.isAuthenticated = Boolean(name);
   }
 
   get identifiers() {
     if (!this.identityMap.has('sub')) {
-      this.identityMap.set('sub', this.credentials.identityId);
+      const sub = this.credentials.session.getIdToken().decodePayload().sub;
+
+      this.identityMap.set('sub', sub);
+      this.identityMap.set('username', this.credentials.user.getUsername());
+      this.identityMap.set('jwt', this.credentials.session.getAccessToken().getJwtToken());
     }
     return this.identityMap;
   }
 }
 
 export default class CognitoIdentityProvider implements IIdentityProvider<AWSServiceCredentials> {
-  constructor(
-    private userPool: CognitoUserPool,
-    private identityPoolId: string,
-    private store: IIdentityStore<OpaqueObject>,
-    private storeKey = 'cognito'
-  ) {}
-
-  private get loginMapId() {
-    const {
-      config: { region },
-    } = AWS;
-    return `cognito-idp.${region}.amazonaws.com/${this.userPool.getUserPoolId()}`;
-  }
+  constructor(private userPool: CognitoUserPool) {}
 
   async getIdentity() {
     const user = this.userPool.getCurrentUser();
@@ -57,12 +45,7 @@ export default class CognitoIdentityProvider implements IIdentityProvider<AWSSer
     }
     try {
       const session = await this.getSession(user);
-      const credentials = this.configureAWSCredentials(session);
-
-      if (credentials.needsRefresh()) {
-        await this.refreshAWSCredentials(credentials);
-      }
-      return new AWSIdentity(user.getUsername(), credentials);
+      return new AWSIdentity(user.getUsername(), { user, session });
     } catch (e) {
       console.log(e);
       return new Identity(null, null);
@@ -77,8 +60,6 @@ export default class CognitoIdentityProvider implements IIdentityProvider<AWSSer
     // This will clear any existing sessions with this userpool and
     // update the currently stored tokens.
     user.setSignInUserSession(session);
-    // Clear any cached credentials.
-    AWS.config.credentials = undefined;
     return this.getIdentity();
   }
 
@@ -87,34 +68,12 @@ export default class CognitoIdentityProvider implements IIdentityProvider<AWSSer
     if (user) {
       user.signOut();
     }
-    AWS.config.credentials = undefined;
   }
 
   private getSession(user: CognitoUser) {
     return new Promise<CognitoUserSession>((resolve, reject) => {
       user.getSession((err: any, session: CognitoUserSession) => {
         err ? reject(err) : resolve(session);
-      });
-    });
-  }
-
-  private configureAWSCredentials(session: CognitoUserSession) {
-    if (!AWS.config.credentials) {
-      AWS.config.credentials = new CognitoIdentityCredentials({
-        IdentityPoolId: this.identityPoolId,
-        Logins: {},
-      });
-    }
-    // See https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityCredentials.html
-    const credentials = AWS.config.credentials as CognitoIdentityCredentials;
-    (credentials.params as any).Logins[this.loginMapId] = session.getIdToken().getJwtToken();
-    return credentials;
-  }
-
-  private refreshAWSCredentials(credentials: CognitoIdentityCredentials) {
-    return new Promise((resolve, reject) => {
-      credentials.refresh(err => {
-        err ? reject(err) : resolve();
       });
     });
   }
